ed14afb74cbf87045cf9366888e10e377dde24ba
tomascarreira <carreira_1012@hotmail.com>
2023-04-17 11:18:18 +0100

    Code formated with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,50 @@
 use std::{net::SocketAddr, str::FromStr};
 
-use http_body_util::Full;
-use hyper::{server, service, Request, body, Response, StatusCode};
 use anyhow::Context;
 use clap::Parser;
+use http_body_util::Full;
+use hyper::{body, server, service, Request, Response, StatusCode};
 
-async fn handle_request(req: Request<body::Incoming>) -> anyhow::Result<Response<Full<body::Bytes>>> {
+async fn handle_request(
+    req: Request<body::Incoming>,
+) -> anyhow::Result<Response<Full<body::Bytes>>> {
     println!("{:?}", req);
     let status_code = match req.headers().get("riposte-request") {
         Some(val) => val,
         None => {
             println!("No risposte-request header on the request.");
-            return Ok(Response::builder().status(StatusCode::BAD_REQUEST).body(Full::new(body::Bytes::new()))?);
-        },
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Full::new(body::Bytes::new()))?);
+        }
     };
 
     let status_code = match status_code.to_str() {
         Ok(val) => val,
         Err(_) => {
             println!("Header value of riposte-request as non ascii characters.");
-            return Ok(Response::builder().status(StatusCode::BAD_REQUEST).body(Full::new(body::Bytes::new()))?);
-        },
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Full::new(body::Bytes::new()))?);
+        }
     };
 
     let status_code = match StatusCode::try_from(status_code) {
         Ok(val) => val,
         Err(_) => {
-            println!("Header value of riposte-request is not a number or is not in the range 100-999.");
-            return Ok(Response::builder().status(StatusCode::BAD_REQUEST).body(Full::new(body::Bytes::new()))?);
-        },
+            println!(
+                "Header value of riposte-request is not a number or is not in the range 100-999."
+            );
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Full::new(body::Bytes::new()))?);
+        }
     };
 
-    Ok(Response::builder().status(status_code).header("riposte-response", status_code.as_str()).body(Full::new(body::Bytes::new()))?)
+    Ok(Response::builder()
+        .status(status_code)
+        .header("riposte-response", status_code.as_str())
+        .body(Full::new(body::Bytes::new()))?)
 }
 
 #[derive(Parser, Debug)]
@@ -48,11 +61,16 @@ async fn main() -> anyhow::Result<()> {
 
     let addr = format!("127.0.0.1:{}", args.port);
     let addr = SocketAddr::from_str(&addr).context("Port given is not a valid port.")?;
-    let listener = tokio::net::TcpListener::bind(addr).await.context("Could not bind to port.")?;
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .context("Could not bind to port.")?;
     println!("Listening on address: {addr}");
 
     loop {
-        let (stream, _) = listener.accept().await.context("Connection was not accepted.")?;
+        let (stream, _) = listener
+            .accept()
+            .await
+            .context("Connection was not accepted.")?;
         println!("Connection accepted.");
 
         tokio::task::spawn(async move {
